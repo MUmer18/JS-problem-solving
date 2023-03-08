96d5700193ed34a7da4aac33ea0f02ed1c18510c
MUmer18 <webomi123@gmail.com>
2023-03-09 00:39:10 +0500

    java script chapter 9

---
+++ chap9.js
@@ -0,0 +1,4 @@
+let firstName = prompt("first name is ");
+let lastName = prompt("last name is ");
+let age = prompt("age is ");
+alert("Hell " + firstName + " " + lastName + " you are " + age + " years old");
